6f37b31838bf14943c83c57dbd66a7b393290c85
Gabriel Teixeira <xcross341@gmail.com>
2025-04-10 12:18:27 -0300

    design base board

--- src/App.js
+++ src/App.js
@@ -1,9 +1,102 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world with React and HarryPhamDev</h1>
-      </header>
+    <div className="trello-master">
+      <nav className="navbar app">App bar </nav>
+      <nav className="navbar board">Board bar </nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/refs/heads/master/trello/img-design.png"/>
+              Design & Research
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>fourth</li>
+            <li>fifth</li>
+            <li>sixth</li>
+            <li>seventh</li>
+            <li>eighth</li>
+            <li>ninth</li>
+            <li>tenth</li>
+            <li>eleventh</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/refs/heads/master/trello/img-design.png"/>
+              Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/refs/heads/master/trello/img-design.png"/>
+              Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/refs/heads/master/trello/img-design.png"/>
+              Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/refs/heads/master/trello/img-design.png"/>
+              Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/refs/heads/master/trello/img-design.png"/>
+              Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/refs/heads/master/trello/img-design.png"/>
+              Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
